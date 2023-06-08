54428a124457d38966af2ff71c8ca2ec5c488344
Wallacee22 <128102264+Wallacee22@users.noreply.github.com>
2023-06-07 21:37:27 -0300

    Add files via upload

---
+++ cadastrar.php
@@ -0,0 +1,14 @@
+<html>
+
+<body>
+    <center>
+<form id="cadastro" action="cadastro.php" method="POST">
+  nome: <input type="text" name="nome" required><br><br>
+  login: <input type="text" name="login" required><br><br>
+  senha: <input type="password" name="senha" required><br><br>
+  <input type="submit" id="cadastrar" value="cadastrar">
+</form>
+
+    </center>
+</body>
+</html>
