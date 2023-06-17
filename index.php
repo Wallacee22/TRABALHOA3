d49bde0b1e67bdf2d07f18f2ba34330daf4d5b8f
Wallacee22 <128102264+Wallacee22@users.noreply.github.com>
2023-06-16 21:27:49 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,21 @@ session_start();
 
 <html>
 <body>
+<style>
+  body{
+font-family: Arial, Helvetica, sans-serif;
+background: linear-gradient(45deg, gray, black);
+text-align: center;
+color: white;
+  }
+  .a{
+    position:absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    background-color: rgba(0,0,0,0);
+  }
+ </style>
 <center>
     olá,<?php
     if(isset($_SESSION['nome'])==null){
@@ -13,6 +28,8 @@ session_start();
        echo $_SESSION['nome'];?>
        <a href="cadastrar.php">Cadastrar</a><br>
        <a href="logout.php">Sair</a>
+       <a href="alterarsenha.php">alterar senha</a><br>
+        <a href="lista.php">lista de usuários</a><br>
        <?php } ?>
 <center>
 </body>
