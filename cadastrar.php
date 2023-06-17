0a19f83ed507149b59a6790771b190121a608b21
Wallacee22 <128102264+Wallacee22@users.noreply.github.com>
2023-06-16 21:26:31 -0300

    Add files via upload

--- cadastrar.php
+++ cadastrar.php
@@ -1,6 +1,21 @@
 <html>
 
 <body>
+<style>
+  body{
+font-family: Arial, Helvetica, sans-serif;
+background: linear-gradient(45deg, gray, black);
+text-align: center;
+color: white;
+  }
+  .a{
+    position:absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    background-color: rgba(0,0,0,0);
+  }
+ </style>
     <center>
 <form id="cadastro" action="cadastro.php" method="POST">
   nome: <input type="text" name="nome" required><br><br>
